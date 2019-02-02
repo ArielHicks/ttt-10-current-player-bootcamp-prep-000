75d65b5de788c4f61bf6c708477021f86cae25db
Ariel Hicks <hicks.ariel@gmail.com>
2019-02-02 19:09:50 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def turn_count(board)
   board.count{|token| token == "X" || token == "O"}
 end
 
-# def current_player(board)
+def current_player(board)
 #   turn_count.even? "X" : "O"
 #
-# end
+end
